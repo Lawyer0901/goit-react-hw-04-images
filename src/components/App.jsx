71323a91cc6a33f73e24dce9be6c677457fbc1cb
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2023-01-18 21:08:45 +0200

    need fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,108 +1,193 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { getImages, PER_PAGE } from './service/API';
+import { fetchImages, PER_PAGE } from './service/API';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 
-export class App extends Component {
-  state = {
-    search: '',
-    images: [],
-    status: 'idle',
-    page: 1,
-  };
+export function App() {
+  const [searchQuerry, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { page, search } = this.state;
-
-    try {
-      if (prevState.search !== search || prevState.page !== page) {
-        this.setState({ status: 'pending' });
-      }
-      if (prevState.search !== search) {
-        const images = await getImages(page, search);
-        if (!images.totalHits) {
+  useEffect(() => {
+    if (isFirstLoad) {
+      return;
+    }
+    setStatus('pending');
+    fetchImages({ page, searchQuerry })
+      .then(({ totalHits, hits }) => {
+        if (hits.length === 0) {
           throw new Error('We have nothing for this query');
         }
+        if (page > 1) {
+          const totalPages = Math.ceil(totalHits / PER_PAGE);
 
-        this.setState({
-          images: [
-            ...images.hits.map(({ id, webformatURL, largeImageURL, tags }) => {
-              return { id, webformatURL, largeImageURL, tags };
-            }),
-          ],
-          status: 'resolved',
-        });
-        return;
-      }
-      if (prevState.page !== page && page !== 1) {
-        const images = await getImages(page, search);
-        this.setState({
-          images: [
-            ...prevState.images,
-            ...images.hits.map(({ id, webformatURL, largeImageURL, tags }) => {
-              return { id, webformatURL, largeImageURL, tags };
-            }),
-          ],
-          status: 'resolved',
-        });
-        if (
-          images.totalHits === this.state.images.length ||
-          images.hits.length < PER_PAGE
-        ) {
-          throw new Error('You loaded all images');
+          setImages(prevImages => {
+            return [
+              ...prevImages,
+              ...hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+                return { id, webformatURL, largeImageURL, tags };
+              }),
+            ];
+          });
+          setStatus('resolved');
+          if (totalPages === page) {
+            throw new Error('You loaded all images');
+          }
+          return;
         }
-        return;
-      }
-    } catch (error) {
-      console.log(error);
 
-      this.setState({ status: 'rejected' });
+        setImages(
+          hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+            return { id, webformatURL, largeImageURL, tags };
+          })
+        );
+        setStatus('resolved');
+        return;
+      })
+      .catch(error => {
+        console.log(error);
+        setStatus('rejected');
+      });
+    return;
+  }, [searchQuerry, page, isFirstLoad]);
+
+  const handleSubmit = async search => {
+    if (searchQuerry === search) {
+      return;
     }
-  }
-
-  handleSubmit = async search => {
-    this.setState(prevState => {
-      if (prevState.search === search) {
-        return;
-      }
-      return { images: [], page: 1, search };
-    });
+    setIsFirstLoad(false);
+    setImages([]);
+    setPage(1);
+    setSearch(search);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  handleClickOnImage = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  };
-
-  // Рендер всех элементов
-
-  render() {
-    const { images, status } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {images.length > 0 && (
-          <ImageGallery images={images} onClick={this.handleClickOnImage} />
-        )}
-        {images.length === 0 && status === 'rejected' && (
-          <div>
-            <img
-              width="400px"
-              src="https://www.meme-arsenal.com/memes/7a2e9f67e58e929277003ca53ab4d1cb.jpg"
-              alt="Nothung to show"
-            />
-          </div>
-        )}
-        {images.length > 0 &&
-          images.length >= PER_PAGE &&
-          status === 'resolved' && <Button loadMore={this.handleLoadMore} />}
-        {status === 'pending' && <Loader />}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {!isFirstLoad && images.length > 0 && <ImageGallery images={images} />}
+      {images.length === 0 && status === 'rejected' && (
+        <div>
+          <img
+            width="400px"
+            src="https://www.meme-arsenal.com/memes/7a2e9f67e58e929277003ca53ab4d1cb.jpg"
+            alt="Nothung to show"
+          />
+        </div>
+      )}
+      {!isFirstLoad && images.length >= PER_PAGE && status === 'resolved' && (
+        <Button loadMore={handleLoadMore} />
+      )}
+      {status === 'pending' && <Loader />}
+    </>
+  );
 }
+
+// export class oldApp extends Component {
+//   state = {
+//     search: '',
+//     images: [],
+//     status: 'idle',
+//     page: 1,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { page, search } = this.state;
+
+//     try {
+//       if (prevState.search !== search || prevState.page !== page) {
+//         this.setState({ status: 'pending' });
+//       }
+//       if (prevState.search !== search) {
+//         const images = await getImages(page, search);
+//         if (!images.totalHits) {
+//           throw new Error('We have nothing for this query');
+//         }
+
+//         this.setState({
+//           images: [
+//             ...images.hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+//               return { id, webformatURL, largeImageURL, tags };
+//             }),
+//           ],
+//           status: 'resolved',
+//         });
+//         return;
+//       }
+//       if (prevState.page !== page && page !== 1) {
+//         const images = await getImages(page, search);
+//         this.setState({
+//           images: [
+//             ...prevState.images,
+//             ...images.hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+//               return { id, webformatURL, largeImageURL, tags };
+//             }),
+//           ],
+//           status: 'resolved',
+//         });
+//         if (
+//           images.totalHits === this.state.images.length ||
+//           images.hits.length < PER_PAGE
+//         ) {
+//           throw new Error('You loaded all images');
+//         }
+//         return;
+//       }
+//     } catch (error) {
+//       console.log(error);
+
+//       this.setState({ status: 'rejected' });
+//     }
+//   }
+
+//   handleSubmit = async search => {
+//     this.setState(prevState => {
+//       if (prevState.search === search) {
+//         return;
+//       }
+//       return { images: [], page: 1, search };
+//     });
+//   };
+
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   handleClickOnImage = () => {
+//     this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+//   };
+
+//   // Рендер всех элементов
+
+//   render() {
+//     const { images, status } = this.state;
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.handleSubmit} />
+//         {images.length > 0 && (
+//           <ImageGallery images={images} onClick={this.handleClickOnImage} />
+//         )}
+//         {images.length === 0 && status === 'rejected' && (
+//           <div>
+//             <img
+//               width="400px"
+//               src="https://www.meme-arsenal.com/memes/7a2e9f67e58e929277003ca53ab4d1cb.jpg"
+//               alt="Nothung to show"
+//             />
+//           </div>
+//         )}
+//         {images.length > 0 &&
+//           images.length >= PER_PAGE &&
+//           status === 'resolved' && <Button loadMore={this.handleLoadMore} />}
+//         {status === 'pending' && <Loader />}
+//       </>
+//     );
+//   }
+// }
--- src/components/service/API.js
+++ src/components/service/API.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '31605839-858af090e8e0e31dbfed95a6b';
 export const PER_PAGE = 12;
 
-export const getImages = async (page, search) => {
+export const fetchImages = async (page, search) => {
   const response = await axios.get(
     `?q=${search}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
   );
