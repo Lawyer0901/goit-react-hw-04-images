845a1a3b020da8435f8cc1c19ec317e9fa9d3bef
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2023-01-19 22:02:24 +0200

    fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,19 @@
 import { Vortex } from 'react-loader-spinner';
+import { Container } from '../Container/Container';
 export function Loader() {
   return (
-    <Vortex
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="vortex-loading"
-      wrapperStyle={{}}
-      wrapperClass="vortex-wrapper"
-      colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
-    />
+    <Container>
+      <Container>
+        <Vortex
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+        />
+      </Container>
+    </Container>
   );
 }
